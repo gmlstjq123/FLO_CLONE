40fc4e69299a34b7f642ea77e09f9662295e6dc3
gmlstjq123 <gmlstjq123@naver.com>
2023-09-22 01:10:22 +0900

    1주차. 하단 네비게이션 바, Fragment 전환

---
+++ app/src/main/java/com/chrome/umcflo/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.chrome.umcflo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chrome.umcflo.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding : FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.homeAlbumImgIv1.setOnClickListener {
+            (context as MainActivity)
+                .supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, AlbumFragment()).commitAllowingStateLoss()
+        }
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/chrome/umcflo/Song.kt
@@ -0,0 +1,9 @@
+package com.chrome.umcflo
+
+data class Song(
+    val title : String = "",
+    val singer : String = ""
+//    val startTime : String = "",
+//    val endTime : String = "",
+//    val isplaying : Boolean = false
+)
